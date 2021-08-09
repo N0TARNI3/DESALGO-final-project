1e9a72f27f1d4a7f785b6152efd796d527a2fdaa
RNI <mail.arniefraga@gmail.com>
2021-08-09 08:43:00 +0800

    Final Dev

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -28754,6 +28754,7 @@ var map
 var routeLayerGroup = L.layerGroup()
 var latlng
 var newItem
+var newItemCount = 0
 
 //import json thru ajax request
 $.when(
@@ -28775,15 +28776,20 @@ $.when(
 function initialize(points){
   map = L.map('map').setView([14.5995, 120.9842], 14);
 
+  var southWest = L.latLng(14.5533,120.9414),
+  northEast = L.latLng(14.6607,121.0285),
+  boundary = L.latLngBounds(southWest, northEast);
+
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
+    minZoom: 14,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
       'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox/streets-v11',
     tileSize: 512,
-    zoomOffset: -1
+    zoomOffset: -1,    
   }).addTo(map);
-
+  map.setMaxBounds(boundary);
   var pathfinder = Router.createPathFinder(roads, map)
 
   //plot testing centers on the map
@@ -28804,7 +28810,6 @@ function initialize(points){
   }).addTo(map);
 
   var newNodePopup = function(latlng) {
-    console.log(latlng)
     return '<div class="p-2"><h4><strong>Add New Waypoint</strong></h4>' +
     '<label for="node_name">Waypoint Name:</label><input type="text" name="node_name" class="form-control" id="node_name" required>'+
     '<label for="node_name">Latitude:</label><input type="text" name="node_lat" class="form-control" id="node_lat" value="'+latlng.lat+'" readonly>'+
@@ -28832,9 +28837,9 @@ function onEachFeature(feature, layer) {
 
 //function that adds new nodes
 $(document).on("click", "#addNewNode", function(){
+  newItemCount++
   var node_name = document.getElementById("node_name").value
   var node_coords = latlng
-  console.log(node_coords)
   map.closePopup();
 
   //append new node to vertices json
@@ -28844,8 +28849,18 @@ $(document).on("click", "#addNewNode", function(){
                         'coordinates': [node_coords.lng, node_coords.lat],
                         }
   feature['properties'] =  {'name': node_name}
-  console.log(feature)
   vertices.features.push(feature)
+
+  //reset form
+  var defaultForm = '<div class="form-group">' +                    
+      '<label>Your Location:</label>' +
+      '<div class="typeahead__container">' +
+          '<div class="typeahead__field">' +
+              '<div class="typeahead__query"><input class="js-typeahead'+newItemCount+'" name="q" autocomplete="off"></div>' +
+              '<div class="typeahead__button"><button type="submit"><span class="typeahead__search-icon"></span></button></div>' +
+          '</div></div></div>'
+  $('#autocompleteForm').html(defaultForm)
+
   //reset nodes
   VerticesAutocomplete(vertices)
 })
@@ -28870,27 +28885,13 @@ function VerticesAutocomplete (vertices) {
     })
   })
   $.typeahead({
-    input: '.js-typeahead',
+    input: '.js-typeahead'+newItemCount,
     minLength: 1,
     hint: true,
     order: 'desc',
     source: {
       data: vertexNames
-    },    
-    /*multiselect: {
-      limit: 1,
-      limitTemplate: 'You can\'t select more than 1 starting location',
-      matchOn: ['id'],
-      cancelOnBackspace: true,
-      callback: {
-        onClick: function (node, item, event) {
-          console.log(item)
-        },
-        onCancel: function (node, item, event) {
-          console.log(item.display + ' Removed!')
-        }
-      }
-    },*/
+    },
     template: function (query, item) {
       var color = '#777'
       return '<span class="list-group">' +
@@ -28899,20 +28900,19 @@ function VerticesAutocomplete (vertices) {
     },
     callback: {
       onClick: function(node, a, item, event) {
-        console.log(item)
         newItem = item
       },
       onSubmit: function (node, form, item, event) {
-        console.log("ONSUBMIT TRIGGER")
-        //reset map every submit
+        //reset map and modal every submit
         map.removeLayer(routeLayerGroup)
         routeLayerGroup.clearLayers()
+        $("#distances table tbody").html("")
+        $('#shortestPath').html("")
+        $('#distanceMatrix tbody').html("")
+        $('#computationLogs').html("")
 
         event.preventDefault()
-        if(!item) {
-          item = newItem
-        }                
-        console.log(item)   
+        item = newItem     
         var start = vertices.features[item.id]
         var distances = []
 
@@ -28937,6 +28937,14 @@ function VerticesAutocomplete (vertices) {
         $('#nearest-center h4').html(distances[0].name)
         $('#nearest-center p').html("Distance: "+distances[0].distance.toFixed(2)+"km")
         $('#nearest-center h4.heading').html("Nearest COVID Testing Center:")
+
+        //display distances on modal
+        distances.map(function(val, i){
+          $("#distances table tbody").append("<tr>"+
+                "<td>"+distances[i].name+"</td>"+
+                "<td>"+distances[i].distance.toFixed(2)+"km</td>"+
+            "</tr>")
+        })
         
         //find shortest path to nearest COVID Testing Center
         var finish = vertices.features[distances[0].id]
@@ -29005,6 +29013,32 @@ var Router = {
         var start = pathfinder._keyFn(roundCoord(wps[i - 1].geometry.coordinates, pathfinder._precision))
         var finish = pathfinder._keyFn(roundCoord(wp.geometry.coordinates, pathfinder._precision))
         var returnedPath = Dijkstra(pathfinder._graph.compactedVertices,start, finish)
+
+        //display shortest path
+        returnedPath.path.map(function (wp, i, wps) {
+          if(i==0) {            
+            $('#shortestPath').append("<strong>"+returnedPath.path[i] + " (Start) </strong> &rarr; ")
+          } else if (i == returnedPath.path.length-1){
+            $('#shortestPath').append("<strong>"+returnedPath.path[i] + " (End)</strong>")
+          }else{
+            $('#shortestPath').append(returnedPath.path[i] + " &rarr; ")
+          }
+        })
+
+        //display distance matrix
+        returnedPath.path.map(function (wp, i, wps) {
+          if(i==0) {            
+            $('#distanceMatrix tbody').append("<tr class='bg-dark'><td colspan='2' class='text-center text-light'><strong>"+wp+":</strong> (Start)</td></tr>")
+          } else if (i == returnedPath.path.length-1){
+            $('#distanceMatrix tbody').append("<tr class='bg-dark'><td colspan='2' class='text-center text-light'><strong>"+wp+"</strong> (End)</td></tr>")
+          }else{
+            $('#distanceMatrix tbody').append("<tr class='bg-dark'><td colspan='2' class='text-center text-light'><strong>"+wp+":</strong></td></tr>")
+          }
+          for (const [child, distance] of Object.entries(pathfinder._graph.compactedVertices[wp])) {
+            $('#distanceMatrix tbody').append('<tr><td>'+child+'</td><td>'+distance+'</td></tr>')
+          }
+        })
+
         //map compacted vertices into actual vertices
         var mappedPath = Router.mapPath(returnedPath, finish)
         // change initial node to starting node
@@ -29077,6 +29111,7 @@ const shortestDistanceNode = (distances, visited) => {
 
 //Dijkstra algorithm
 const Dijkstra = (graph, startNode, endNode) => {
+  $("#computationLogs").append("<p><strong>-----START NODE: "+startNode+"-----</strong></p>")
 	// establish object for recording distances from the start node
 	let distances = {};
 	distances[endNode] = "Infinity";
@@ -29103,33 +29138,33 @@ const Dijkstra = (graph, startNode, endNode) => {
 		for (let child in children) {
 			// make sure each child node is not the start node
 			if (String(child) === String(startNode)) {
-				//console.log("don't return to the start node! 🙅");
 				continue;
 			} else {
-				//console.log("startNode: " + startNode); 
-				//console.log("distance from node " + parents[node] + " to node " + node + ")");
-				//console.log("previous distance: " + distances[node]);
 				// save the distance from the start node to the child node
 				let newdistance = distance + children[child];
-				//console.log("new distance: " + newdistance);
 				// if there's no recorded distance from the start node to the child node in the distances object
 				// or if the recorded distance is shorter than the previously stored distance from the start node to the child node
 				// save the distance to the object
 				// record the path
-				if (!distances[child] || distances[child] > newdistance) {
+				if (!distances[child]) {
 					distances[child] = newdistance;
 					parents[child] = node;
-					//console.log("distance + parents updated");
-				} else {
-					//console.log("not updating, because a shorter path already exists!");
-				}
+				} else if(distances[child] > newdistance){
+          $("#computationLogs").append("<p>Distance from Start Node  to node " + node +"</p>")
+          $("#computationLogs").append("<p>SHORTER PATH EXIST! Route Updated</p>")
+          $("#computationLogs").append("<p>Previous Distance: " +distances[child]+"</p>")
+					distances[child] = newdistance;
+					parents[child] = node;
+          $("#computationLogs").append("<p>New Distance: " + newdistance+"</p>")
+        }
 			}
 		}
 		// move the node to the visited set
 		visited.push(node);
 		// move to the nearest neighbor node
 		node = shortestDistanceNode(distances, visited);
-	}
+	}  
+  $("#computationLogs").append("<p><strong>-----END NODE REACHED!-----</strong></p>")
 
 	// using the stored paths from start node to end node
 	// record the shortest path
--- scripts/script.js
+++ scripts/script.js
@@ -22,6 +22,7 @@ var map
 var routeLayerGroup = L.layerGroup()
 var latlng
 var newItem
+var newItemCount = 0
 
 //import json thru ajax request
 $.when(
@@ -43,15 +44,20 @@ $.when(
 function initialize(points){
   map = L.map('map').setView([14.5995, 120.9842], 14);
 
+  var southWest = L.latLng(14.5533,120.9414),
+  northEast = L.latLng(14.6607,121.0285),
+  boundary = L.latLngBounds(southWest, northEast);
+
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
+    minZoom: 14,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
       'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox/streets-v11',
     tileSize: 512,
-    zoomOffset: -1
+    zoomOffset: -1,    
   }).addTo(map);
-
+  map.setMaxBounds(boundary);
   var pathfinder = Router.createPathFinder(roads, map)
 
   //plot testing centers on the map
@@ -72,7 +78,6 @@ function initialize(points){
   }).addTo(map);
 
   var newNodePopup = function(latlng) {
-    console.log(latlng)
     return '<div class="p-2"><h4><strong>Add New Waypoint</strong></h4>' +
     '<label for="node_name">Waypoint Name:</label><input type="text" name="node_name" class="form-control" id="node_name" required>'+
     '<label for="node_name">Latitude:</label><input type="text" name="node_lat" class="form-control" id="node_lat" value="'+latlng.lat+'" readonly>'+
@@ -100,9 +105,9 @@ function onEachFeature(feature, layer) {
 
 //function that adds new nodes
 $(document).on("click", "#addNewNode", function(){
+  newItemCount++
   var node_name = document.getElementById("node_name").value
   var node_coords = latlng
-  console.log(node_coords)
   map.closePopup();
 
   //append new node to vertices json
@@ -112,8 +117,18 @@ $(document).on("click", "#addNewNode", function(){
                         'coordinates': [node_coords.lng, node_coords.lat],
                         }
   feature['properties'] =  {'name': node_name}
-  console.log(feature)
   vertices.features.push(feature)
+
+  //reset form
+  var defaultForm = '<div class="form-group">' +                    
+      '<label>Your Location:</label>' +
+      '<div class="typeahead__container">' +
+          '<div class="typeahead__field">' +
+              '<div class="typeahead__query"><input class="js-typeahead'+newItemCount+'" name="q" autocomplete="off"></div>' +
+              '<div class="typeahead__button"><button type="submit"><span class="typeahead__search-icon"></span></button></div>' +
+          '</div></div></div>'
+  $('#autocompleteForm').html(defaultForm)
+
   //reset nodes
   VerticesAutocomplete(vertices)
 })
@@ -138,7 +153,7 @@ function VerticesAutocomplete (vertices) {
     })
   })
   $.typeahead({
-    input: '.js-typeahead',
+    input: '.js-typeahead'+newItemCount,
     minLength: 1,
     hint: true,
     order: 'desc',
@@ -153,20 +168,19 @@ function VerticesAutocomplete (vertices) {
     },
     callback: {
       onClick: function(node, a, item, event) {
-        console.log(item)
         newItem = item
       },
       onSubmit: function (node, form, item, event) {
-        console.log("ONSUBMIT TRIGGER")
-        //reset map every submit
+        //reset map and modal every submit
         map.removeLayer(routeLayerGroup)
         routeLayerGroup.clearLayers()
+        $("#distances table tbody").html("")
+        $('#shortestPath').html("")
+        $('#distanceMatrix tbody').html("")
+        $('#computationLogs').html("")
 
         event.preventDefault()
-        if(!item) {
-          item = newItem
-        }                
-        console.log(item)   
+        item = newItem     
         var start = vertices.features[item.id]
         var distances = []
 
@@ -191,6 +205,14 @@ function VerticesAutocomplete (vertices) {
         $('#nearest-center h4').html(distances[0].name)
         $('#nearest-center p').html("Distance: "+distances[0].distance.toFixed(2)+"km")
         $('#nearest-center h4.heading').html("Nearest COVID Testing Center:")
+
+        //display distances on modal
+        distances.map(function(val, i){
+          $("#distances table tbody").append("<tr>"+
+                "<td>"+distances[i].name+"</td>"+
+                "<td>"+distances[i].distance.toFixed(2)+"km</td>"+
+            "</tr>")
+        })
         
         //find shortest path to nearest COVID Testing Center
         var finish = vertices.features[distances[0].id]
@@ -259,6 +281,32 @@ var Router = {
         var start = pathfinder._keyFn(roundCoord(wps[i - 1].geometry.coordinates, pathfinder._precision))
         var finish = pathfinder._keyFn(roundCoord(wp.geometry.coordinates, pathfinder._precision))
         var returnedPath = Dijkstra(pathfinder._graph.compactedVertices,start, finish)
+
+        //display shortest path
+        returnedPath.path.map(function (wp, i, wps) {
+          if(i==0) {            
+            $('#shortestPath').append("<strong>"+returnedPath.path[i] + " (Start) </strong> &rarr; ")
+          } else if (i == returnedPath.path.length-1){
+            $('#shortestPath').append("<strong>"+returnedPath.path[i] + " (End)</strong>")
+          }else{
+            $('#shortestPath').append(returnedPath.path[i] + " &rarr; ")
+          }
+        })
+
+        //display distance matrix
+        returnedPath.path.map(function (wp, i, wps) {
+          if(i==0) {            
+            $('#distanceMatrix tbody').append("<tr class='bg-dark'><td colspan='2' class='text-center text-light'><strong>"+wp+":</strong> (Start)</td></tr>")
+          } else if (i == returnedPath.path.length-1){
+            $('#distanceMatrix tbody').append("<tr class='bg-dark'><td colspan='2' class='text-center text-light'><strong>"+wp+"</strong> (End)</td></tr>")
+          }else{
+            $('#distanceMatrix tbody').append("<tr class='bg-dark'><td colspan='2' class='text-center text-light'><strong>"+wp+":</strong></td></tr>")
+          }
+          for (const [child, distance] of Object.entries(pathfinder._graph.compactedVertices[wp])) {
+            $('#distanceMatrix tbody').append('<tr><td>'+child+'</td><td>'+distance+'</td></tr>')
+          }
+        })
+
         //map compacted vertices into actual vertices
         var mappedPath = Router.mapPath(returnedPath, finish)
         // change initial node to starting node
@@ -331,6 +379,7 @@ const shortestDistanceNode = (distances, visited) => {
 
 //Dijkstra algorithm
 const Dijkstra = (graph, startNode, endNode) => {
+  $("#computationLogs").append("<p><strong>-----START NODE: "+startNode+"-----</strong></p>")
 	// establish object for recording distances from the start node
 	let distances = {};
 	distances[endNode] = "Infinity";
@@ -357,33 +406,33 @@ const Dijkstra = (graph, startNode, endNode) => {
 		for (let child in children) {
 			// make sure each child node is not the start node
 			if (String(child) === String(startNode)) {
-				//console.log("don't return to the start node! 🙅");
 				continue;
 			} else {
-				//console.log("startNode: " + startNode); 
-				//console.log("distance from node " + parents[node] + " to node " + node + ")");
-				//console.log("previous distance: " + distances[node]);
 				// save the distance from the start node to the child node
 				let newdistance = distance + children[child];
-				//console.log("new distance: " + newdistance);
 				// if there's no recorded distance from the start node to the child node in the distances object
 				// or if the recorded distance is shorter than the previously stored distance from the start node to the child node
 				// save the distance to the object
 				// record the path
-				if (!distances[child] || distances[child] > newdistance) {
+				if (!distances[child]) {
 					distances[child] = newdistance;
 					parents[child] = node;
-					//console.log("distance + parents updated");
-				} else {
-					//console.log("not updating, because a shorter path already exists!");
-				}
+				} else if(distances[child] > newdistance){
+          $("#computationLogs").append("<p>Distance from Start Node  to node " + node +"</p>")
+          $("#computationLogs").append("<p>SHORTER PATH EXIST! Route Updated</p>")
+          $("#computationLogs").append("<p>Previous Distance: " +distances[child]+"</p>")
+					distances[child] = newdistance;
+					parents[child] = node;
+          $("#computationLogs").append("<p>New Distance: " + newdistance+"</p>")
+        }
 			}
 		}
 		// move the node to the visited set
 		visited.push(node);
 		// move to the nearest neighbor node
 		node = shortestDistanceNode(distances, visited);
-	}
+	}  
+  $("#computationLogs").append("<p><strong>-----END NODE REACHED!-----</strong></p>")
 
 	// using the stored paths from start node to end node
 	// record the shortest path
